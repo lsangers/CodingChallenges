d04fbd7be5fb0feb9111d532516325d2e664eece
Loek Sangers <loek.sangers@hu.nl>
2022-02-16 15:11:33 +0000

    adding base code from Codingame

--- UltimateTicTacToe/bot.cpp
+++ UltimateTicTacToe/bot.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+
+    // game loop
+    while (1) {
+        int opponent_row;
+        int opponent_col;
+        cin >> opponent_row >> opponent_col; cin.ignore();
+        int valid_action_count;
+        cin >> valid_action_count; cin.ignore();
+        for (int i = 0; i < valid_action_count; i++) {
+            int row;
+            int col;
+            cin >> row >> col; cin.ignore();
+        }
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+        cout << "0 0" << endl;
+    }
 }
